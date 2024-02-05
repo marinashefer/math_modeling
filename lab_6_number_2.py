0ee0c6a161e9fecb97f9eb727dfa1438993e3a0a
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-02-05 23:45:09 +0300

    Update lab_6_number_2.py
    mde

---
+++ lab_6_number_2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+
+k = int(input('предел изменения переменной x:'))
+n = int(input('количество точек N, на которое разбиваются соответствующие пределы:'))
+
+y = k/x
+def x 
