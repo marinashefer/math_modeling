b93fce2f5d9ba766b499ca051f25db3349d0125a
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-03-05 14:08:30 +0200

    yp8g0

--- lab_7_number_2.py
+++ lab_7_number_2.py
@@ -2,21 +2,19 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np 
 
-def circle_move(R, vx0, vy0, time, r):
-    x0 = vx0 * time
-    y0 = vy0 * time
-    alpha = np.arange(0, 2*np.pi, 0.01)
-    r = alpha * time
-    x = x0 + R * np.cos(alpha)
-    y = y0 + R * np.sin(alpha)
+def circle_move(time):
+    R = 0.02 * time 
+    alpha = np.arange(0, 2*np.pi, 0.1)
+    x = R * np.cos(alpha)
+    y = R * np.sin(alpha)
     return x, y
 
 def animate(i):
-    ball.set_data(circle_move(R=0.5, vx0 = 0.01, vy0 = 0.01, time = i))
+    ball.set_data(circle_move(time = i))
 
 if __name__ == "__main__":
     fig, ax = plt.subplots()
-    ball, = plt.plot([], [], 'o', color = 'g', label = 'Ball')
+    ball, = plt.plot([], [], 'o', color = 'r', label = 'Ball')
 
     edge = 3
     plt.axis('equal')
