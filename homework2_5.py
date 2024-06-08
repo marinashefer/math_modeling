2498eefc18b54b2391c766cc37f5960ebf01f5c5
marinashefer <78643165+marinashefer@users.noreply.github.com>
2024-06-08 16:58:56 +0300

    comm

--- homework2_5.py
+++ homework2_5.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('weaher.csv')
+data = pd.read_csv('weather.csv')
 
-data['temperature'].hist(bins = 2)
-data.plot()
+data['temperature'].plot(kind = 'hist', bins = 2)
+plt.savefig('temperature.png')
